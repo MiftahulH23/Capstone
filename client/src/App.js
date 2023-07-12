6c7be12af9cff910f5bf6b7ae95ced64760901d3
hibatillah <hibatillah21si@mahasiswa.pcr.ac.id>
2023-07-12 17:34:35 +0700

    create login signup function

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,58 @@
-
-import './App.css';
-import Login from './pages/login'
-import React, { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/navbar'
-import Header from './components/header';
-import Beranda from './pages/beranda';
-import PesananSaya from './pages/PesananSaya';
-import TentangKami from './pages/TentangKami';
-import Persediaan from './pages/Persediaan';
-import Menu from './pages/Menu';
-import Dashboard from './pages/Dashboard';
-import InputPesanan from './pages/InputPesanan';
-import DashboardSupplier from './pages/DashboardSupplier';
-import PersediaanSupplier from './pages/PersediaanSupplier';
-import Pesananmasuk from './pages/Pesananmasuk';
-import Product from './pages/Product';
-import Bahanbaku from './pages/Bahanbaku';
-import Inputproduk from './pages/Inputproduk';
-import Inputbahanbaku from './pages/Inputbahanbaku';
-import Inputbahanbakusupplier from './pages/Inputbahanbakusupplier';
-
+import "./App.css";
+import Login from "./pages/login";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar";
+import Header from "./components/header";
+import Beranda from "./pages/beranda";
+import PesananSaya from "./pages/PesananSaya";
+import TentangKami from "./pages/TentangKami";
+import Persediaan from "./pages/Persediaan";
+import Menu from "./pages/Menu";
+import Dashboard from "./pages/Dashboard";
+import InputPesanan from "./pages/InputPesanan";
+import DashboardSupplier from "./pages/DashboardSupplier";
+import PersediaanSupplier from "./pages/PersediaanSupplier";
+import Pesananmasuk from "./pages/Pesananmasuk";
+import Product from "./pages/Product";
+import Bahanbaku from "./pages/Bahanbaku";
+import Inputproduk from "./pages/Inputproduk";
+import Inputbahanbaku from "./pages/Inputbahanbaku";
+import Inputbahanbakusupplier from "./pages/Inputbahanbakusupplier";
+import Register from "./pages/register";
 
 function App() {
-  const [login, setLogin] = useState(true)
-  const handleLogin = () => setLogin(login)
+  const [login, setLogin] = useState(false);
+  const handleLogin = () => setLogin(!login);
 
-  const [user, setUser] = useState("admin");
+  const [user, setUser] = useState({
+    id: "",
+    name: "",
+    role: "",
+  });
+  const handleUser = (id, name, role) =>
+    setUser({
+      id: id,
+      name: name,
+      role: role,
+    });
 
   return (
     <Router>
-      <div className={`w-full h-screen  bg-background ${!login? 'grid place-items-center' : 'flex'}`}>
+      <div
+        className={`w-full h-screen  bg-background ${
+          !login ? "grid place-items-center" : "flex"
+        }`}
+      >
         {!login ? (
           <Routes>
-            <Route path='/' element={<Login />} />
+            <Route
+              path="/"
+              element={
+                <Login handleLogin={handleLogin} handleUser={handleUser} />
+              }
+            />
+            <Route path="/register" element={<Register />} />
           </Routes>
         ) : (
           <>
@@ -43,30 +62,37 @@ function App() {
             <div className="flex-auto h-screen overflow-y-scroll ">
               <Header />
               <Routes>
-                {user === "admin"? (
+                {user === "admin" ? (
                   <>
-                  <Route path='/' element={<Dashboard />} />
-                  <Route path='/product' element={<Product />} />
-                  <Route path='/bahanbaku' element={<Bahanbaku />} />
-                  <Route path='/inputpesanan' element={<InputPesanan />} />
-                  <Route path='/inputproduk' element={<Inputproduk />} />
-                  <Route path='/inputbahanbaku' element={<Inputbahanbaku />} />
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="/product" element={<Product />} />
+                    <Route path="/bahanbaku" element={<Bahanbaku />} />
+                    <Route path="/inputpesanan" element={<InputPesanan />} />
+                    <Route path="/inputproduk" element={<Inputproduk />} />
+                    <Route
+                      path="/inputbahanbaku"
+                      element={<Inputbahanbaku />}
+                    />
                   </>
-                ):user === "supplier"? (
+                ) : user === "supplier" ? (
                   <>
-                  <Route path='/' element={<DashboardSupplier />} />
-                  <Route path='/persediaansupplier' element={<PersediaanSupplier />} />
-                  <Route path='/inputbahanbakusupplier' element={<Inputbahanbakusupplier />} />
-                  <Route path='/pesananmasuk' element={<Pesananmasuk />} />
-
+                    <Route path="/" element={<DashboardSupplier />} />
+                    <Route
+                      path="/persediaansupplier"
+                      element={<PersediaanSupplier />}
+                    />
+                    <Route
+                      path="/inputbahanbakusupplier"
+                      element={<Inputbahanbakusupplier />}
+                    />
+                    <Route path="/pesananmasuk" element={<Pesananmasuk />} />
                   </>
-
-                ):(
+                ) : (
                   <>
-                  <Route path='/' element={<Beranda />} />
-                  <Route path='/menu' element={<Menu />} />
-                  <Route path='/pesanan' element={<PesananSaya />} />
-                  <Route path='/tentang' element={<TentangKami />} />
+                    <Route path="/" element={<Beranda />} />
+                    <Route path="/menu" element={<Menu />} />
+                    <Route path="/pesanan" element={<PesananSaya />} />
+                    <Route path="/tentang" element={<TentangKami />} />
                   </>
                 )}
               </Routes>
--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,23 +1,85 @@
-import Logo from '../assets/img/logo.png'
-const Login = () => {
-    return (
-        <div className="grid grid-cols-2 w-[70%] h-[70%] bg-white py-20 divide-x divide-slate-700 ">
-            {/* Kiri */}
-            <div className='grid place-items-center'>
-                <img src={Logo} alt="" />
-            </div>
-            {/* Kanan */}
-            <div className='grid place-items-center'>
-                <div className='flex flex-col w-[50%]'>
-                    <h1 className='text-center font-bold text-2xl'>LOGIN</h1>
-                    <p className='place-items-start'>Email</p>
-                    <input type="text" className='border-2 w-full px-4 py-1 rounded-lg' placeholder='Email' />
-                    <p className='mt-2'>Password</p>
-                    <input type="text" className='border-2 w-full px-4 py-1 rounded-lg' placeholder='Password' />
-                    <button className='bg-primary w-full mt-8 rounded-lg px-4 py-2 text-white'>LOGIN</button>
-                </div>
-            </div>
-        </div>
-    )
-}
-export default Login
+import { Link, useNavigate } from "react-router-dom";
+import Logo from "../assets/img/logo.png";
+import { GetData } from "../components/Api";
+
+const Login = ({ handleLogin, handleUser }) => {
+  const navigate = useNavigate();
+  const User = () => {
+    const { users } = GetData("http://localhost:5000/user");
+    console.log(users);
+    return users;
+  };
+  const dataUser = User();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const data = {
+      email: e.target.email.value,
+      password: e.target.password.value,
+    };
+
+    if (
+      dataUser?.data.some(
+        (item) => item.email === data.email && item.password === data.password
+      )
+    ) {
+      const user = dataUser.data.find(
+        (item) => item.email === data.email && item.password === data.password
+      );
+      handleLogin();
+      handleUser(user._id, user.name, user.role);
+      console.log("login success");
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    } else {
+      alert("Email atau password salah");
+    }
+  };
+
+  return (
+    <div className="grid grid-cols-2 w-[70%] h-[70%] bg-white py-20 divide-x divide-slate-700 ">
+      {/* Kiri */}
+      <div className="grid place-items-center">
+        <img src={Logo} alt="" />
+      </div>
+      {/* Kanan */}
+      <div className="grid place-items-center">
+        <form onSubmit={handleSubmit} className="flex flex-col w-[50%]">
+          <h1 className="text-center font-bold text-2xl">LOGIN</h1>
+          <label htmlFor="email" className="">
+            Email
+          </label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            className="border-2 w-full px-4 py-1 rounded-lg"
+            placeholder="Input email"
+          />
+          <label htmlFor="password" className="mt-2">
+            Password
+          </label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            className="border-2 w-full px-4 py-1 rounded-lg"
+            placeholder="Input password"
+          />
+          <button className="bg-primary w-full mt-8 rounded-lg px-4 py-2 text-white">
+            LOGIN
+          </button>
+          <div className="mt-3">
+            Belum punya akun?{" "}
+            <Link to={"/register"} className="text-primary">
+              Daftar
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+export default Login;
