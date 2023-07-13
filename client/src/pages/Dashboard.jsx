b85b6f7188dbdd55ad9c6c454c0220c606f03e7f
MiftahulH23 <miftahul21si@mahasiswa.pcr.ac.id>
2023-07-14 05:53:36 +0700

    update dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,12 +4,15 @@ import { CupIcon } from "../components/Svg";
 const Dashboard = () => {
   return (
     <div className='px-10 py-8'>
-      <div className='grid grid-cols-3 gap-6 mb-10'>
+      <iframe width="100%" height="1200" src="https://lookerstudio.google.com/embed/reporting/2f34b724-42a6-4a42-87af-2ae7f4c4a524/page/p_f7t8w80u7c" frameborder="0" allowfullscreen></iframe>
+
+
+      {/* <div className='grid grid-cols-3 gap-6 mb-10'>
         <ScoreCard judul={"Menu Terlaris"} ket={"Dalam Bulan ini"} icon={<CupIcon/>} subjudul={"Esteh Susu Nusantara"} desc={"Total penjualan:350"} />
         <ScoreCard judul={"Menu Terlaris"} ket={"Dalam Bulan ini"} icon={<CupIcon/>} subjudul={"Esteh Susu Nusantara"} desc={"Total penjualan:350"} />
         <ScoreCard judul={"Menu Terlaris"} ket={"Dalam Bulan ini"} icon={<CupIcon/>} subjudul={"Esteh Susu Nusantara"} desc={"Total penjualan:350"} />
       </div>
-      <GrafikCard />
+      <GrafikCard /> */}
     </div>
   )
 }
--- client/src/pages/DashboardSupplier.jsx
+++ client/src/pages/DashboardSupplier.jsx
@@ -3,55 +3,9 @@ import { ScoreCard } from "../components/Card";
 import { CupIcon } from "../components/Svg";
 
 const DashboardSupplier = () => {
-    const data = []
   return (
     <div className="px-10 py-8">
-      <div className="grid grid-cols-3 gap-6">
-        <ScoreCard
-          judul={"Menu Terlaris"}
-          ket={"Dalam Bulan ini"}
-          icon={<CupIcon />}
-          subjudul={"Esteh Susu Nusantara"}
-          desc={"Total penjualan:350"}
-        />
-        <ScoreCard
-          judul={"Menu Terlaris"}
-          ket={"Dalam Bulan ini"}
-          icon={<CupIcon />}
-          subjudul={"Esteh Susu Nusantara"}
-          desc={"Total penjualan:350"}
-        />
-        <ScoreCard
-          judul={"Menu Terlaris"}
-          ket={"Dalam Bulan ini"}
-          icon={<CupIcon />}
-          subjudul={"Esteh Susu Nusantara"}
-          desc={"Total penjualan:350"}
-        />
-      </div>
-      <p className="py-4 text-lg font-medium">Daftar Pengiriman Bahan Baku</p>
-      <div className="bg-white aspect-[4/1] px-5 py-3">
-        <table className="table-auto w-full">
-          <thead>
-            <tr>
-              <th>Nama</th>
-              <th>Jumlah</th>
-              <th>Keterangan</th>
-              <th>Aksi</th>
-            </tr>
-          </thead>
-          <tbody>
-            {data?.map((item) => (
-              <tr>
-                <td>{item.name ?? "-"}</td>
-                <td>{item.jumlah ?? "-"}</td>
-                <td>{item.keterangan ?? 0}</td>
-                <td></td>
-              </tr>
-            )) ?? <tr>Produk Tidak tersedia</tr>}
-          </tbody>
-        </table>
-      </div>
+      <iframe width="100%" height="1200" src="https://lookerstudio.google.com/embed/reporting/2f34b724-42a6-4a42-87af-2ae7f4c4a524/page/p_f7t8w80u7c" frameborder="0" allowfullscreen></iframe>
     </div>
   );
 };
